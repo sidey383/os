cb00b8923bc916292e1dbe67385fddd8dc48400d
sidey383 <sidey383@mail.ru>
2023-09-27 16:39:54 +0700

    return gettid(); from thread function

--- threads/thread.c
+++ threads/thread.c
@@ -5,10 +5,13 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void *mythread(void *arg) {
 	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
-	return (void*)42;
+	pid_t* ret = malloc(sizeof(pid_t));
+    (*ret) = gettid();
+    return ret;
 }
 
 int main() {
@@ -16,21 +19,23 @@ int main() {
 	int err;
 
 	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+    while (1) {
+	    err = pthread_create(&tid, NULL, mythread, NULL);
+	    if (err != 0) {
+	        fprintf(stderr, "main: pthread_create() failed: %s\n", strerror(err));
+		    return -1;
+	    } else {
+            printf("main: create thread %ld\n", tid);
+        }
 
-	err = pthread_create(&tid, NULL, mythread, NULL);
-	if (err != 0) {
-	    fprintf(stderr, "main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	} else {
-        printf("main: create thread %ld\n", tid);
-    }
-
-    long val;
-    err = pthread_join(tid, (void**)&val);
-    if (err != 0) {
-        fprintf(stderr, "main: pthread_join() failed: %s\n", strerror(err));
-    } else {
-        printf("main: join thread %ld return %ld\n", tid, val);
+        pid_t* val;
+        err = pthread_join(tid, (void**)&val);
+        if (err != 0) {
+            fprintf(stderr, "main: pthread_join() failed: %s\n", strerror(err));
+        } else {
+            printf("main: join thread %ld return %d\n", tid, *val);
+            free(val);
+        }
     }
 
 	return 0;
