ab384f06a5bbe3f5851d7b46c284da5bf28e5cfb
sidey383 <sidey383@mail.ru>
2023-10-04 18:30:39 +0700

    d

--- threads/thread.c
+++ threads/thread.c
@@ -8,20 +8,7 @@
 
 void *thread1(void *arg) {
     printf("thread1 [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
-    return NULL;
-}
-
-void *thread2(void* arg) {
-    printf("thread2 [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
-    return NULL;
-}
-
-
-int main() {
-    pthread_t tid;
-    int err;
     sigset_t set;
-
 
     sigemptyset(&set);
     sigaddset(&set, SIGQUIT);
@@ -31,8 +18,25 @@ int main() {
         fprintf(stderr, "main: pthread_sigmask() failed: %s\n", strerror(err));
         return -1;
     }
+    return NULL;
+}
+
+void *thread2(void* arg) {
+    printf("thread2 [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+    return NULL;
+}
+
+void *thread3(void* arg) {
+    printf("thread3 [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+    return NULL;
+}
+
+
+int main() {
+    pthread_t tid;
+    int err;
     printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
-    err = pthread_create(&tid, NULL, mythread, NULL);
+    err = pthread_create(&tid, NULL, thread1, NULL);
     if (err != 0) {
         fprintf(stderr, "main: pthread_create() failed: %s\n", strerror(err));
         return -1;
